2334d7d8b61e5bb9ef756939c79791d08ed48332
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-09-27 22:12:17 +0300

    update: add read and write file functions

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const buffer = await fs.readFile(PATH_DB, 'utf-8');
+    return buffer;
+  } catch (err) {
+    console.error(err.message);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    fs.writeFile(PATH_DB, updatedContacts, 'utf-8');
+  } catch (err) {
+    console.error(err.message);
+  }
+};
