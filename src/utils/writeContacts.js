8cd8c3d4312b489ea1fe776995ee5cf71d2bd9d0
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-09-27 22:38:41 +0300

    fix: json formatting

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -3,7 +3,7 @@ import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
   try {
-    const data = JSON.stringify(updatedContacts);
+    const data = JSON.stringify(updatedContacts, undefined, 2);
     fs.writeFile(PATH_DB, data, 'utf-8');
   } catch (err) {
     console.error(err.message);
